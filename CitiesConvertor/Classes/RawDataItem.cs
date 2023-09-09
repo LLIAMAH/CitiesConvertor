3c8820a0c18052dba1d004c19432b6747426d2eb
WildSquirrl13 <wildsquirrl@gmail.com>
2023-09-09 20:40:33 +0300

    added csv reading, preparer, write to data base

---
+++ CitiesConvertor/Classes/RawDataItem.cs
@@ -0,0 +1,46 @@
+﻿using Cities.DB.Entities;
+using CitiesConvertor.Classes;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitiesConvertor.Classes
+{
+    public class RawDataItem
+    {
+        public string city { get; set; }
+        public string city_ascii { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string country { get; set; }
+        public string iso2 { get; set; }
+        public string iso3 { get; set; }
+        public string admin_name { get; set; }
+        public string capital { get; set; }
+        public string population { get; set; }
+        public string id { get; set; }
+
+    }
+
+}
+public class RawDataItemMap : ClassMap<RawDataItem>
+{
+    public RawDataItemMap()
+    {
+        Map(p => p.city).Index(0);
+        Map(p => p.city_ascii).Index(1);
+        Map(p => p.lat).Index(2);
+        Map(p => p.lng).Index(3);
+        Map(p => p.country).Index(4);
+        Map(p => p.iso2).Index(5);
+        Map(p => p.iso3).Index(6);
+        Map(p => p.admin_name).Index(7);
+        Map(p => p.capital).Index(8);
+        Map(p => p.population).Index(9);
+        Map(p => p.id).Index(10);
+    }
+}
--- CitiesConvertor/Program.cs
+++ CitiesConvertor/Program.cs
@@ -1,10 +1,15 @@
 ﻿using Cities.DB;
 using Cities.DB.Entities;
+using CitiesConvertor.Classes;
+using CsvHelper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.IO;
 
 using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration(cfg => { cfg.AddJsonFile("appsettings.json"); })
@@ -23,6 +28,50 @@ using IHost host = Host.CreateDefaultBuilder(args)
     })
     .Build();
 
+var FilePath = "C:\\Users\\Squirrel\\Downloads\\simplemaps_worldcities_basicv1.76\\worldcities.csv";
+
+//using (StreamReader reader = new StreamReader(FilePath))
+//{
+//    string? line;
+//    while ((line = await reader.ReadLineAsync()) != null)
+//    {
+//        var split = line.Split(',');
+//        if (split.Length != 11)
+//        {
+//            throw new Exception("Размер рабитой строки не валиден!");
+//        }
+        
+//        var record = new RawDataItem()
+//        {
+//            city = split[0],
+//            city_ascii = split[1],
+//            lat = ConvertToDouble(split[2]),
+//            lng = ConvertToDouble(split[3]),
+//            country = split[4],
+//            iso2 = split[5],
+//            iso3 = split[6],
+//            admin_name = split[7],
+//            capital = split[8],
+//            population = ConvertToInt(split[9]),
+//            id = ConvertToLong(split[10])
+//        };
+//    }
+//}
+using (var reader = new StreamReader(FilePath))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    csv.Context.RegisterClassMap<RawDataItemMap>();
+    var records = csv.GetRecords<RawDataItem>();
+    var countries = records.Select(o=>o.country).Distinct().ToList();
+    var capitals = records.Select(o=>o.capital).Distinct().ToList();
+    var t = records;
+}
+
+
+
+
+
+
 using (var ctx = host.Services.GetService<AppDbCtx>())
 {
     /*
@@ -34,4 +83,23 @@ using (var ctx = host.Services.GetService<AppDbCtx>())
 
     ctx.SaveChanges();
     */
+}
+
+int ConvertToInt (string data)
+{
+    var converted = 0;
+    int.TryParse(data, out converted);
+    return converted;
+}
+double ConvertToDouble(string data)
+{
+    double converted = 0;
+    double.TryParse(data, CultureInfo.InvariantCulture, out converted);
+    return converted;
+}
+long ConvertToLong(string data)
+{
+    long converted = 0;
+    long.TryParse(data, CultureInfo.InvariantCulture, out converted);
+    return converted;
 }
